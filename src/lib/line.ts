3a665c62f12fe39e314fb744d0ec563f620e5ffe
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-07-31 12:37:23 +0700

    Delete src/lib/line.ts

--- src/lib/line.ts
+++
@@ -1,6 +0,0 @@
-import { LineDappSDK } from '@linenext/dapp-portal-sdk';
-
-export const sdk = new LineDappSDK({
-  dappId: process.env.NEXT_PUBLIC_LINE_DAPP_ID || '', // pastikan DAPP_ID ada di .env
-  chainId: 137, // Ganti sesuai kebutuhan (137 = Polygon Mainnet, 80001 = Mumbai)
-});
