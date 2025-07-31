a7dd06cb998dc328ceedf63dfa0eb58eab503e6d
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-07-31 11:17:16 +0700

    Update LoginButton.tsx

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -3,6 +3,7 @@
 import sdk from "@linenext/dapp-portal-sdk";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { supabase } from "@/lib/supabaseClient";
 
 export default function LoginButton() {
   const [walletType, setWalletType] = useState("");
@@ -17,9 +18,22 @@ export default function LoginButton() {
 
   const handleLogin = async () => {
     const walletProvider = sdk.getWalletProvider();
+
     try {
-      await walletProvider.connect(); // Proses login ke wallet
-      router.push("/home"); // Redirect ke halaman utama setelah login
+      await walletProvider.connect();
+      const address = await walletProvider.getAddress();
+
+      // Simpan ke Supabase (jika belum ada)
+      await supabase.from("users").upsert(
+        {
+          wallet_address: address,
+          points: 0,
+          energy: 200,
+        },
+        { onConflict: "wallet_address" } // supaya tidak dobel
+      );
+
+      router.push("/home");
     } catch (error) {
       console.error("Login gagal:", error);
     }
